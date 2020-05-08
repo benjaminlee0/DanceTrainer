3b3e12a21c2ea58d7059d60adad0b6b19a6bb55f
benjaminlee0 <benjamin_lee@ucsb.edu>
2020-05-07 17:12:23 -0700

    Updates

---
+++ precache-manifest.e2b7b50e7eb7a0249ebc5fc5f9f0d9af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75696b7baa333948dff4cfac017f9496",
+    "url": "/DanceTrainer/index.html"
+  },
+  {
+    "revision": "83f4e3cefd84b652e3ef",
+    "url": "/DanceTrainer/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5faf2600a1293f84471c",
+    "url": "/DanceTrainer/static/js/2.9449a77e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DanceTrainer/static/js/2.9449a77e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83f4e3cefd84b652e3ef",
+    "url": "/DanceTrainer/static/js/main.bc5cbbc1.chunk.js"
+  },
+  {
+    "revision": "6b9e680eec351769e221",
+    "url": "/DanceTrainer/static/js/runtime-main.d7d6d4e4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/DanceTrainer/static/media/logo.5d5d9eef.svg"
+  }
+]);
